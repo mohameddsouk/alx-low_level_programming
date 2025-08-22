b6134f699fe3e0af0db6104fc2b4ed295212d1cc
mohameddsouk <mohameddsouk@gmail.com>
2025-08-22 15:42:52 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
-#include<stdio.h> 
+#include<stdio.h>
+
 /**
- *main - Entry point
+ * main - Entry point
  *
- *Return: Alawys 0 (success)
- */
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0 (success)
+*/
+
 int main(void)
-{	
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+{
+	puts(""\"Programming is like building a multilingual puzzle");
+	return (0);
 }

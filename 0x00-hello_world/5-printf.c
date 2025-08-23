4915468fb12b585ddbd0e773f798d83b0c7522ca
mohameddsouk <mohameddsouk@gmail.com>
2025-08-23 02:16:21 -0700

    good

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,7 +6,6 @@
  *
  * Return: Alawys 0 (success)
 */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");

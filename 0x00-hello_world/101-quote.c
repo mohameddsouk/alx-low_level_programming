c2b50bfaefe04473d5c3c46b231e0bd4d9adcb3c
mohameddsouk <mohameddsouk@gmail.com>
2025-08-25 09:56:28 -0700

    good

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1 (not success)
+*/
+
+int main(void)
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quo, 59);
+	return (1);
+}

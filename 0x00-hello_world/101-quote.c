344b8bd9a6b70b59ffc0848ad28d10e97d03d6b6
mohameddsouk <mohameddsouk@gmail.com>
2025-08-26 10:17:48 -0700

    me

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,16 @@
 /**
  * main - Entry point
  *
+ * Description: print a qoute using write function
+ * write(int fd.const void *buf.size_t count)
+ *
  * Return: 1 (not success)
 */
 
 int main(void)
-{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(1, quo, 59);
 	return (1);
 }
